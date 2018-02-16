5dbbbd9d0609c9eeb92b77ad63b5f6882428ebbb
Araggar <lucas.mayr.athayde@gmail.com>
2018-02-16 18:52:11 -0200

    Add remaining methods

--- lolbuilds.py
+++ lolbuilds.py
@@ -6,48 +6,74 @@ import os
 from sys import argv, exit
 
 class builder:
-	def main(self, role):
 
-		if not os.path.exists('champions.json'):
+	def __init__(self):
+		
+		if os.path.exists('champions.json'):
+			with open('champions.json', 'r') as champ_file:
+				champs = json.load(champ_file)
+
+			self.top = champs[0]
+			self.jungle = champs[1]
+			self.mid = champs[2]
+			self.adc = champs[3]
+			self.support = champs[4]
+
+			
+		else:
 			with open("champions.json", 'w') as champ_file:
 				json.dump([[],[],[],[],[]],champ_file)
+
+			self.top = []
+			self.jungle = []
+			self.mid = []
+			self.adc = []
+			self.support = []
 		
-
-
-		self.top = []
-		self.jungle = []
-		self.mid = []
-		self.adc = []
-		self.support = []
-
+	def add(self, *_):
+		inp = input("Select role to Add champions <top, jungle, mid, support, adc>\n>>> ")
+		if inp not in ('top', 'jungle', 'mid', 'adc', 'support'):
+			print("Not a valid role")
+			return
+		champ = input("Adding champions to {}\nType <exit> to leave\n>>> ".format(inp))
+		while champ != 'exit':
+			getattr(self, inp).append(champ)
+			champ = input(">>> ")
+		with open("champions.json", 'w') as champ_file:
+				json.dump([self.top, self.jungle, self.mid, self.adc, self.support],champ_file)
+		self.__init__()
 		
-		if role in ('add', 'Add'):
-			inp = input("Select role to Add champions <top, jungle, mid, support, adc>\n>>> ")
-			if inp not in ('top', 'jungle', 'mid', 'adc', 'support'):
-				print("Not a valid role")
-				exit(1)
-			champ = input("Adding champions to {}\nType <exit> to leave\n>>> ".format(inp))
-			while champ != 'exit':
-				getattr(self, inp).append(champ)
-				champ = input(">>> ")
-			exit(0)
-
-
-
+	def remove(self, *_):
+		inp = input("Select role to Add champions <top, jungle, mid, support, adc>\n>>> ")
+		if inp not in ('top', 'jungle', 'mid', 'adc', 'support'):
+			print("Not a valid role")
+			return
+		champ = input("Adding champions to {}\nType <exit> to leave\n>>> ".format(inp))
+		while champ != 'exit':
+			try:
+				getattr(self, inp).remove(champ)
+			except Exception:
+				print("Champion Not Found!")
+			champ = input(">>> ")
+		with open("champions.json", 'w') as champ_file:
+				json.dump([self.top, self.jungle, self.mid, self.adc, self.support],champ_file)
+		self.__init__()
+		
+	def list(self, *_):
+		inp = input("Select role to Add champions <top, jungle, mid, support, adc>\n>>> ")
+		print("\n-----{}-----\n".format(inp.upper()))
+		for champ in getattr(self, inp):
+			print(champ)
+		print("\n-----END-----\n")
+		
+	def build(self, inp, *_):
 		try:
-			champions = {
-				'top' : self.top,
-				'jungle' : self.jungle,
-				'mid' : self.mid,
-				'adc' : self.adc,
-				'support' : self.support
-			}
-			champion = random.choice(champions[role])
-		except KeyError:
-			champion, role = role, ''
+			champion = random.choice(getattr(self, inp))
+		except AttributeError:
+			champion, inp = inp, ''
 		except IndexError:
-			print("Role <{}> is empty!".format(role))
-			exit(1)
+			print("Role <{}> is empty!".format(inp))
+			return
 
 
 		addr = "http://www.probuilds.net/champions/details/{}".format(champion)
@@ -57,9 +83,10 @@ class builder:
 		popular = re.findall("'item-name gold'>([\w\s']+?)<\/div>[\w\W]+?'popularity green'>([\d\.]+?%)+?<\/div>",html.text)
 
 		if not popular:
-			print("Invalid Champion/Role <{}>".format(role))
-			exit(1)
+			print("Invalid Champion/Role <{}>".format(inp))
+			return
 
+		print("\n{}".format(champion.upper()))
 		print("\n---ITEMS---\n")
 		for i, items in enumerate(popular):
 			if i in [6, 10]:
@@ -69,23 +96,35 @@ class builder:
 			print("{} --- {}".format(items[0], items[1]))
 
 
-		addr_runes = "http://champion.gg/champion/{}/{}?".format(champion, role)
+		addr_runes = "http://champion.gg/champion/{}/{}?".format(champion, inp)
 		html = session.get(addr_runes)
 		runes = re.findall('color=[\w\W]+?>([\w ]+?)<\/div>',html.text)
 		print("\n---RUNES---\n")
 		for i, rune in enumerate(runes):
 			print() if i == 8 else None
 			print(rune)
+	
+	def exit(self, *_):
+		print("Goodbye!")
+		exit(0)
+	
+	def main(self, *_):
+
+		while True:
+			inp = input("\n\n\nOptions are:\nadd\nremove\nlist\nexit\n\nType the champion name/role to get a build\n>>> ")
+			try:
+				getattr(self, inp, self.build)(inp)
+			except Exception:
+				self.build(inp)
+		
+
 
 
 if __name__ == '__main__':
 	os.chdir(os.path.dirname(os.path.realpath(__file__)))
 	try:
-		if argv[1] == '-h':
-			raise IndexError
-		builder().main(argv[1])
-	except IndexError:
-		print("Usage\npython {} Champion/Role/Add/Remove/Show".format(argv[0]))
-		exit(1)
-
+		builder().main()
+	except KeyboardInterrupt:
+		print("Goodbye!")
+		exit(0)
 
